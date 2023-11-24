2fec2fe0425ddcd1cd9f1f3d85288b43d7c6a687
rindawork <nirotaku@gmail.com>
2023-11-24 18:27:50 -0400

    first commit

---
+++ Semana1/Program.cs
@@ -0,0 +1,3 @@
+﻿//Crea un algoritmo que imprima el area de un circulo. El programa debe de preguntar el radio al usuario
+
+
---
+++ Semana2/Program.cs
@@ -0,0 +1,3 @@
+﻿//Suma los valores dentro de v1 y v2. Imprimelos en pantalla
+int v1 = 3;
+string v2 = "7";
